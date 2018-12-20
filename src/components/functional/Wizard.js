4067b35684bf6b158b893c5b0ffc0318b3ad7d33
Jared Butler <jaredbtlr21@gmail.com>
2018-12-20 09:13:57 -0700

    Began working on modal for adding data to the calendar

--- src/components/functional/Wizard.js
+++ src/components/functional/Wizard.js
@@ -6,17 +6,46 @@ import Modal from './Modal'
 
 class Wizard extends React.Component {
     state = {
-        
+        blockOne: false,
+        blockTwo: false,
+        blockThree: false
     }
-    render(){
-    return (
-        <div>
-            <Modal>
-                <input />
-                <input />
-            </Modal>
-        </div>
-    )
+
+    handleBlockPicker = (event) => {
+        this.setState(prevState => { return { [event.target.type]: !prevState.event.target.type } })
+    };
+
+    displayBlockOne = () => {
+        if (this.state.blockOne) {
+            return (
+                <Modal>
+                    <div>
+                    <p>Company One</p>
+                    <input />
+                    <p>Company Two</p>
+                    <input />
+                    <button>Add To Calendar</button>
+                    </div>
+                    <div>
+                    <button value={this.state.blockOne} onClick={this.handleBlockPicker}>Block 1</button>
+                    <button value={this.state.blockTwo} onClick={this.handleBlockPicker}>Block 2</button>
+                    <button vlaue={this.state.blockThree} onClick={this.handleBlockPicker}>Block 3</button>
+                    </div>
+                </Modal>
+            )
+        }
+    };
+
+    displayBlockTwo = () => { };
+
+    displayBlockThree = () => { };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
     }
 }
 
