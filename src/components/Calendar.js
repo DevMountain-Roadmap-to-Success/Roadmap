5cf63be9810bf510843a12b0ce53e26b5560e93a
Jared Butler <jaredbtlr21@gmail.com>
2018-12-19 17:09:01 -0700

    some dummy appointments hard coded into the calendar for testing

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -21,7 +21,8 @@ class Calendar extends Component {
             <div>
                 <Scheduler
                     data={[
-                        
+                        { startDate: '2018-12-20 10:00', endDate: '2018-12-20 11:00', title: 'Meeting' },
+                        { startDate: '2018-12-21 18:00', endDate: '2018-12-21 19:30', title: 'Go to a gym' }
                     ]}
                 >
                     <WeekView 
