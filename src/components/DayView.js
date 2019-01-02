b50223a621081fe50e8ca67333c48d36b29300cf
randaman23 <randaman234@gmail.com>
2019-01-02 12:21:21 -0700

    calendar setup

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import moment from "moment";
+import TimeSlot from "./TimeSlot";
 
 class DayView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime: 9,
+      endTime: 17,
+      times: [{ time: "", activity: "" }]
+    };
+  }
+
+  componentDidMount() {
+    const { startTime, endTime } = this.state;
+    let times = [];
+    let currentTime = startTime;
+    for (let i = currentTime; i < endTime; i++) {
+      times.push({ time: currentTime, activity: "" });
+      currentTime++;
+    }
+    this.setState({ times });
+  }
+
   render() {
+    console.log(this.state.times);
+    let hours = this.state.times.map((time, i) => {
+      return <TimeSlot key={time} hour={moment(time)} />
+    });
     return (
       <div>
         <h2>{moment(this.props.date, "MM/DD/YY").format("dddd")}</h2>
         <h3>{this.props.date}</h3>
+        {hours}
       </div>
     );
   }
--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -2,21 +2,6 @@ import React, { Component } from "react";
 import moment from "moment";
 
 class TimeSlot extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startTime: 9,
-      endTime: 17
-    };
-  }
-
-  componentDidMount(){
-      let times = []
-      let currentTime = this.state.startTime
-      times.push(moment(currentTime))
-  }
-
-
   render() {
     return (
       <div>
