b1a54a036c5eafc7c01ab9e5d70b19aac26c6449
randaman23 <randaman234@gmail.com>
2019-01-02 14:32:07 -0700

    calendar, db, and endpoint for activities

--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-import moment from "moment";
+// import moment from "moment";
+import axios from "axios";
 
 class TimeSlot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activity: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get('');
+  }
   render() {
     return (
       <div>
-        <h1>Time</h1>
+        <h1>{this.props.time}</h1>
+        <h2>{this.props.activity}</h2>
+        <input type="text" />
+        <button>+</button>
       </div>
     );
   }
