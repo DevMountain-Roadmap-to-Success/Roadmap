75874c2a06b1f06f5b7133692bdfbf282290bdf6
randaman23 <randaman234@gmail.com>
2019-01-02 13:02:59 -0700

    small change

--- src/components/TimeSlot.js
+++ src/components/TimeSlot.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import moment from "moment";
 
 class TimeSlot extends Component {
+  constructor(props){
+    super(props)
+    this.state={}
+  }
   render() {
     return (
       <div>
-        <h1>Time</h1>
+        <h1>{this.props.time}</h1>
       </div>
     );
   }
