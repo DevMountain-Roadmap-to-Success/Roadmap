589cf460254f895f64f0e95af302a02d4d5b7fb8
randaman23 <randaman234@gmail.com>
2019-01-09 16:07:53 -0700

    more for the api

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,5 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
+import styled from 'styled-components'
+
+const Main = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-around;
+border: 1px solid black;
+width: 300px;
+height: 400px;
+
+.button_1{
+  position: fixed;
+  top:10px;
+}
+.answer_btn{
+  position: fixed;
+  top: 250px;
+}
+`
 
 class Trivia extends Component {
   constructor(props) {
@@ -12,24 +32,25 @@ class Trivia extends Component {
   }
   getQuestion = () => {
     axios.get("/api/trivia").then(res => {
-      console.log(res.data[0].question);
+      console.log(res.data[0].question, res.data[0].answer);
       this.setState({
         question: res.data[0].question,
-        answer: res.data[0].answer
+        answer: res.data[0].answer,
+        toggle: false
       });
     });
   };
   getAnswer = () => {
-   this.setState(!this.state.toggle)
+    this.setState({toggle: !this.state.toggle});
   };
   render() {
     return (
-      <div>
-        <button onClick={this.getQuestion}>Get Trivia Question</button>
+      <Main>
+        <button className="button_1" onClick={this.getQuestion}>Get Trivia Question</button>
         <h1>{this.state.question}</h1>
-        <button onClick={this.getAnswer}>Get Answer</button>
-        <h3>{this.state.answer}</h3>
-      </div>
+        <button className="answer_btn" onClick={this.getAnswer}>Get Answer</button>
+        {this.state.toggle === true ? <h3>{this.state.answer}</h3> : null}
+      </Main>
     );
   }
 }
