983cc82d3e19ee6394cadde13895ead077f22515
Jared Butler <jaredbtlr21@gmail.com>
2019-01-10 15:38:42 -0700

    added some notes to the FlashcardMenu component

--- src/components/functional/FlashcardMenu.js
+++ src/components/functional/FlashcardMenu.js
@@ -1,23 +1,24 @@
 import React from 'react';
+//style imports
 import styled from 'styled-components';
 import {Menu} from './ElementStyles';
 
+//styling
 const Menu1 = styled(Menu) `
-    /* top: 30px; */
     position: absolute;
     right: unset;
     left: 8px;
     width: 300px;
-    /* height: 70px; */
     z-index: 100px;
     margin-top: -11px;
 `
+
 
 const FlashcardMenu = (props) => {
     const {data} = props;
 
 
-
+   //This maps all of the menu options into selectable items on the menu.
    let handleDisplay = () => {
         let arr = [];
         for(let i in data){
