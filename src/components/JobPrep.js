20fada199b7960341ae85d1d2e7345071d2a180d
Jared Butler <jaredbtlr21@gmail.com>
2019-01-11 15:55:57 -0700

    added new drag and drop library, used job prep as the tester page.

--- src/components/JobPrep.js
+++ src/components/JobPrep.js
@@ -1,198 +1,71 @@
 import React, { Component } from "react";
-import Draggable, { DraggableCore } from 'react-draggable';
-import Iframe from 'react-iframe';
 import styled from "styled-components";
 import Header from './Header';
-import {connect} from 'react-redux';
-import {getPosition} from './../ducks/reducer';
+import { connect } from 'react-redux';
+import { getPosition } from './../ducks/reducer';
 import Flashcard from './Flashcard';
+import Weather from './widgets/Weather';
+import Gauntlet from './functional/Gauntlet';
 
+var ReactGridLayout = require('react-grid-layout');
 
-const Div = styled.div`
-Div{
-  display: flex;
-width: 100vw;
-}
+const Fiv = styled.div`
 
-/* .box{
+  /* background-color: red; */
   display:flex;
-  height: 470px;
-  width: 670px;
-  background-color: black;
-  border-radius: 5px;
-} */
+  justify-content: space-around;
+`
 
-.box2{
-  display:flex;
-  height: 550px;
-  width: 1050px;
-  background-color: white;
-  border-style: solid;
-  border-width: 20px;
-  border-color: black;
-  border-radius: 5px;
-}
-
-/* .frame{
-  margin-left: 35px;
-  margin-top: 35px;
-} */
-
-.gauntlet{
-  margin-left: 5px;
-  margin-top: 30px;
-}
-
-/* h1{
-  margin-left: 220px;
-  margin-top: 10px;
-  color: white;
-}
-h2{
-  display:flex;
-  justify-content: space-between;
-  margin-left: -190px;
-  margin-top: 445px;
-  color: white;
-} */
-
-.gaunt{
-  margin-left: 440px;
-  margin-top: 10px;
-  color: black;
-  font-weight: bold;
-  font-size: 25px;
-}
-.gaunt2{
-  display:flex;
-  justify-content: space-between;
-  margin-left: -165px;
-  margin-top: 480px;
-  color: black;
-}
+const Gaunt = styled.div`
+background-color:black;
 `
 
 class JobPrep extends Component {
   constructor() {
     super();
-    this.state = {
-      activeDrags: 0,
-      deltaPosition: {
-        x: 0, y: 0
-      },
-      0:{}
-     
-    }
-
-    this.handleDrag = this.handleDrag.bind(this);
 
   };
-
-  handleDrag(e, ui) {
-    const { x, y } = this.state.deltaPosition;
-    this.setState({
-      deltaPosition: {
-        x: x + ui.deltaX,
-        y: y + ui.deltaY,
-      }
-    });
-
-    console.log(e.target.id, 
-      {x: x, y: y,},
-      {x: x + ui.deltaX, y: y + ui.deltaY,},
-      {x: ui.lastX + ui.deltaX, y: ui.lastY + ui.deltaY,},
-      
-      )
-
-  }
-
-  onStart = () => {
-    this.setState({activeDrags: ++this.state.activeDrags});
-  }
-
-  onStop = () => {
-    this.setState({activeDrags: --this.state.activeDrags});
-  }
-  
-
-
   render() {
-    const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
-    const {deltaPosition, controlledPosition} = this.state;
+    const { collision, compact } = this.props
+
+    var layout = [
+      { i: "flashcard", x: 0, y: 0, w: 12.5, h: 3.44, isResizable: false },
+      { i: "weather", x: 13, y: 0, w: 7, h: 2.1, isResizable: false, },
+      { i: "gauntlet", x: 0, y: 3.5, w: 19.3, h: 5.4, isResizable: false, }
+    ]
+
     return (
       <>
-      <Header/>
-      <Div className="page">
-
-     
-
-         <Draggable onDrag={this.handleDrag} {...dragHandlers}
-        defaultPosition={{x:25, y:25}}
-        grid={[10, 10]}
+        <Header />
+        <ReactGridLayout className="layout" layout={layout}
+          style={{ position: `relative` }}
+          cols={36}
+          rows={12}
+          width={2000}
+          height={300}
+          preventCollision={collision}
+          compactType={compact ? 'vertical' : null}
+          autoSize={false}
+          margin={[1, 1]}
         >
+          {/* <Div className="page"> */}
 
-<Flashcard />
-        
-      </Draggable>
+          <Fiv key='flashcard' className='testBox'>
+            <Flashcard />
+          </Fiv>
 
-      {/* <Draggable onDrag={this.handleDrag} {...dragHandlers}
-        defaultPosition={{x:730, y:-445}}
-        grid={[10, 10]}
+          <Fiv key='weather'>
 
-        >
-          <div className="box" id={1}>
-          <h1 id={1}>React / Node Interview Question</h1>
-          
-        <Iframe className="frame"
-        url="https://quizlet.com/350088308/flashcards/embed"
-        width="600px"
-        height="400px"
-        />
-        <h2 id={1}>Click Border to Drag</h2>
-        </div>
-      </Draggable>
-
-      <Draggable onDrag={this.handleDrag} {...dragHandlers}
-        defaultPosition={{x:25, y:-430}}
-        grid={[10, 10]}
-
-        >
-          <div className="box">
-          <h1>React / Node Interview Question</h1>
-          
-        <Iframe className="frame"
-        url="https://quizlet.com/350088308/flashcards/embed"
-        width="600px"
-        height="400px"
-        />
-        <h2>Click Border to Drag</h2>
-        </div>
-      </Draggable>
+            <Weather />
+          </Fiv>
 
 
+          <Gaunt key='gauntlet'>
 
-      <Draggable onDrag={this.handleDrag} {...dragHandlers}
-        defaultPosition={{x:25, y:-420}}
-        grid={[10, 10]}
+            <Gauntlet />
+          </Gaunt>
 
-        >
-          <div className="box2">
-          <h1 className="gaunt">Quiz Yourself</h1>
-          
-        <Iframe className="gauntlet"
-        url="https://gauntlet.surge.sh"
-        width="1000px"
-        height="450px"
-        />
-        <h2 className="gaunt2">Click Borders to Drag</h2>
-        </div>
-      </Draggable> */}
-
-        {/* <iframe src="https://quizlet.com/350088308/flashcards/embed" height="500" width="100%" style="border:0"></iframe> */}
-
-
-
-      </Div>
+        </ReactGridLayout>
       </>
     );
   }
