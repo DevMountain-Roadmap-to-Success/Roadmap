bc25d19739d4e7655db300934686c695ef778044
kguyton6 <38778115+kguyton6@users.noreply.github.com>
2019-11-25 19:23:43 -0700

    Update Alert.js

--- src/components/functional/Alert.js
+++ src/components/functional/Alert.js
@@ -3,15 +3,12 @@ import SweetAlert from 'react-bootstrap-sweetalert'
 import {connect} from 'react-redux'
 import {toggleAlert} from '../../ducks/reducer'
 
-const Alert = (props) => {
-    console.log(props)
-    return (
-        <SweetAlert title={props.title} onConfirm={props.toggleAlert}>
-                <p>{props.children}</p>
+const Alert = ({title, children}) => (
+        <SweetAlert title={title} onConfirm={props.toggleAlert}>
+                <p>{children}</p>
        </SweetAlert>
-
     )
-}
+    
 const mapStateToProps = state => {
     return { alert: state.alert }
 }
